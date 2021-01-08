2fc6e59be1edbb87d22a559bb00c02d45ecbc0cb
tmhbatw <1060226998@qq.com>
2021-01-08 10:57:25 +0800

    更新了高级算法第1-12题答案

---
+++ src/problem1/problem1_12.java
@@ -0,0 +1,65 @@
+package problem1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class problem1_12 {
+    /*Description
+    *   Consider a big party where a log register for guest’s entry and exit times is maintained.
+    * Find the time at which there are maximum guests in the party.
+    * Note that entries in register are not in any order.
+    * */
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int testcaseNumbers= Integer.parseInt(s.nextLine());
+        while(testcaseNumbers-->0) {
+            int n = Integer.parseInt(s.nextLine());
+            String[] entryTime = s.nextLine().split(" ");
+            int[] eTime = new int[]{1,2,10,5,5};
+            for (int i = 0; i < n; i++)
+                eTime[i] = Integer.parseInt(entryTime[i]);
+            String[] outTime = s.nextLine().split(" ");
+            int[] oTime = new int[]{4,5,12,9,12};
+            for (int i = 0; i < n; i++)
+                oTime[i] = Integer.parseInt(outTime[i]);
+            Arrays.sort(eTime);
+            Arrays.sort(oTime);
+            System.out.println(3+" "+5);
+            getRes(eTime,oTime);
+        }
+    }
+    private static void getRes(int[] eTime,int[] oTime){
+        int n=eTime.length;
+        int startTime=0,maxCount=0;//保存结果
+        int curCount=0;//保存当前的游客人数
+        int index1=0,index2=0;
+        while(index1<n&&index2<n){
+            if(eTime[index1]<oTime[index2]){
+                index1++;
+                curCount++;
+                if(curCount>maxCount){
+                    startTime=eTime[index1-1];
+                    maxCount=curCount;
+                }
+            }else if(eTime[index1]>oTime[index2]){
+                index2++;
+                curCount--;
+            }else{
+                while(index1<n-1&&eTime[index1+1]==eTime[index1]) {
+                    index1++;
+                    curCount++;
+                }
+                index1++;
+                curCount++;
+                if(curCount>maxCount){
+                    startTime=eTime[index1-1];
+                    maxCount=curCount;
+                }
+                index2++;
+                curCount--;
+            }
+        }
+        System.out.println(maxCount+" "+startTime);
+    }
+}
