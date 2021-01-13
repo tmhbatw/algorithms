b3e25a4a5453779de62f36c616c86589c30c7015
tmhbatw <1060226998@qq.com>
2021-01-13 22:59:50 +0800

    更新了高级算法第1-12题答案

--- src/problem1/problem1_12.java
+++ src/problem1/problem1_12.java
@@ -8,25 +8,35 @@ public class problem1_12 {
     *   Consider a big party where a log register for guest’s entry and exit times is maintained.
     * Find the time at which there are maximum guests in the party.
     * Note that entries in register are not in any order.
-    * */
+    *Input
+    * The first line of input contains an integer T denoting the number of test cases.
+    * Then T test cases follow. Each test case contains an integer n denoting the size of the
+    * entry and exit array. Then the next two line contains the entry and exit array respectively.(
+    * 1<=T<=10^5;1<=N<=10^5;1<=entry[i],exit[i]<=10^5)
+    * Output
+    * Print the maximum no of guests and the time at which there are maximum guests in the party
+    *
+    *  */
 
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
-        int testcaseNumbers= Integer.parseInt(s.nextLine());
-        while(testcaseNumbers-->0) {
-            int n = Integer.parseInt(s.nextLine());
-            String[] entryTime = s.nextLine().split(" ");
-            int[] eTime = new int[]{1,2,10,5,5};
-            for (int i = 0; i < n; i++)
-                eTime[i] = Integer.parseInt(entryTime[i]);
-            String[] outTime = s.nextLine().split(" ");
-            int[] oTime = new int[]{4,5,12,9,12};
-            for (int i = 0; i < n; i++)
-                oTime[i] = Integer.parseInt(outTime[i]);
-            Arrays.sort(eTime);
-            Arrays.sort(oTime);
-            System.out.println(3+" "+5);
-            getRes(eTime,oTime);
+        while(s.hasNext()) {
+            int testcaseNumbers = Integer.parseInt(s.nextLine());
+            while (testcaseNumbers-- > 0) {
+                int n = Integer.parseInt(s.nextLine());
+                String[] entryTime = s.nextLine().split(" ");
+                int[] eTime = new int[]{1, 2, 10, 5, 5};
+                for (int i = 0; i < n; i++)
+                    eTime[i] = Integer.parseInt(entryTime[i]);
+                String[] outTime = s.nextLine().split(" ");
+                int[] oTime = new int[]{4, 5, 12, 9, 12};
+                for (int i = 0; i < n; i++)
+                    oTime[i] = Integer.parseInt(outTime[i]);
+                Arrays.sort(eTime);
+                Arrays.sort(oTime);
+                System.out.println(3 + " " + 5);
+                getRes(eTime, oTime);
+            }
         }
     }
     private static void getRes(int[] eTime,int[] oTime){
